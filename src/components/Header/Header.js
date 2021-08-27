54c1fe45855b28028e1b6a002868630330b0cad0
Naima Hassan <naima.hassan434@gmail.com>
2021-08-27 12:06:02 +0100

    Header component import and Styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ fontSize: "50px" }}>Blog Star</h1>
+    </div>
+  );
+};
+
+export default Header;
