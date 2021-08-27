6ed33c8e92c2ca112ac1131e528de69d437fb2c0
Naima Hassan <naima.hassan434@gmail.com>
2021-08-27 12:15:48 +0100

    Post List components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import PostList from "./components/Posts/PostList";
 
 function App() {
+  const post1 = {
+    title: "Hello",
+    author: "John",
+    description: "Lorem Ipsome",
+  };
+  const post2 = {
+    title: "Hello Evryone",
+    author: "John Smith",
+    description: "Lorem Ipsome random post",
+  };
+
   return (
     <div className="App">
       <Header />
+      <PostList />
     </div>
   );
 }
---
+++ src/components/Posts/PostList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostList = () => {
+  return (
+    <div>
+      <div>Post 1</div>
+      <div>Post 2</div>
+    </div>
+  );
+};
+
+export default PostList;
