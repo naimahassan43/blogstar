6cc9803ac0476ddcb7eeeab310f56fd8f3d2752a
Naima Hassan <naima.hassan434@gmail.com>
2021-08-27 13:20:18 +0100

    Prop-Types

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Post = (props) => {
+  const { title, author, children } = props;
   return (
     <div>
-      <h2>{props.title}</h2>
-      <h5>{props.author}</h5>
-      {props.children}
+      <h2>{title}</h2>
+      <h5>{author}</h5>
+      {children}
     </div>
   );
 };
 
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
 export default Post;
